f4d126207f43b41a2ecc6679fc34c9be1d2204e5
Shi Hong <shaquanhong@gmail.com>
2025-10-02 16:09:39 -0400

    argon crate added

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+use argon2::{self, Argon2};
 use std;
 
 fn main() {
-    println!("Hello, world!");
     let args: Vec<String> = std::env::args().collect();
     let stringed_args = args.join(" ");
-    println!("You entered: {:?}", stringed_args)
+    println!("You entered: {:?}", stringed_args);
+
+    let arg_entry_pw = args[3].clone();
+    let b_pw: &[u8] = arg_entry_pw.as_bytes();
+    let salt = b"test";
+
+    let mut output_key_material = [0u8; 32];
+    let _ = Argon2::default().hash_password_into(b_pw, salt, &mut output_key_material);
+
+    println!("{:?}", output_key_material);
 }
