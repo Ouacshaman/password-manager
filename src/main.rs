5ac04d7715b2ea7fbb91e9700d413e4bcae9ab74
Shi Hong <shaquanhong@gmail.com>
2025-10-17 20:13:23 -0400

    got clap to work

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 use argon2::Argon2;
 use clap::{Parser, Subcommand};
-use sqlx;
+//use sqlx;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -30,10 +30,10 @@ enum Commands {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
 
-    let conn_str =
-        std::env::var("DATABASE_URL").expect("Database Url is not entered into dotenv file");
+    //let conn_str =
+    //    std::env::var("DATABASE_URL").expect("Database Url is not entered into dotenv file");
 
-    let _pool = sqlx::SqlitePool::connect(&conn_str).await?;
+    //let _pool = sqlx::SqlitePool::connect(&conn_str).await?;
 
     let arg_entry_pw = cli
         .master_pw
@@ -45,6 +45,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let _ = Argon2::default().hash_password_into(b_pw, salt, &mut output_key_material);
 
     println!("{:?}", &output_key_material.to_ascii_lowercase());
+
+    println!("{}:{}", cli.name.unwrap_or_default(), arg_entry_pw,);
 
     Ok(())
 }
