8aee2c5bc3e4b4cbda7f67469b9a57b4b7ec0b36
shi hong <shaquanhong@gmail.com>
2025-10-28 18:34:54 -0400

    add credential function added

--- src/cred/mod.rs
+++ src/cred/mod.rs
@@ -35,3 +35,33 @@ pub async fn get_list(p: &sqlx::SqlitePool, name: String) -> Result<Vec<Creds>, 
 
     Ok(creds)
 }
+
+pub async fn add_cred(
+    p: &sqlx::SqlitePool,
+    name: String,
+    username: String,
+    url: String,
+    nonce: Vec<u8>,
+    secret_cipher: Vec<u8>,
+    ) ->  Result<(), Box<dyn std::error::Error>> {
+    let now = chrono::Local::now().naive_local();
+    let res = sqlx::query(
+        r#"
+INSERT INTO entries(name, username, url, nonce, secret_cipher, created_at, updated_at)
+VALUES(?, ?, ?, ?, ?, ?, ?)
+        "#
+        )
+        .bind(name),
+        .bind(username)
+        .bind(url)
+        .bind(nonce)
+        .bind(secret_cipher)
+        .bind(now)
+        .bind(now)
+        .execute(p)
+        .await?;
+
+    println!("{}". res.rows_affected());
+
+    Ok(())
+}
